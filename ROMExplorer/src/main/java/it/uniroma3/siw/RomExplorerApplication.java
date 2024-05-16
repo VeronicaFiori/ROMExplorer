773768df3c50b7601ae8a0c1da938a4c499cf773
Veronica Fiori <ver.fiori1@stud.uniroma3.it>
2024-05-16 14:25:31 +0200

    inizio

---
+++ ROMExplorer/src/main/java/it/uniroma3/siw/RomExplorerApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomExplorerApplication.class, args);
+	}
+
+}
