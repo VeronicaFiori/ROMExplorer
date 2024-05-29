52f08a19ce12bcd7746b2d2001fce048a5a26d6b
Nadaa3672 <nadahekal13@gmail.com>
2024-05-29 14:56:30 +0200

    modifica

--- ROMExplorer/src/main/resources/import.sql
+++ ROMExplorer/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 insert into tipologia_attrazione (id, nome) values(nextval('tipologia_attrazione_seq'), 'monumento');
 
 insert into attrazione (id, nome, urlimage, prezzo, tipo) values(nextval('attrazione_seq'), 'Colosseo', '\static\images\cittaRoma.jpg', 18.00, (SELECT id FROM tipologia_attrazione WHERE nome='monumento'));
+
